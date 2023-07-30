6785c44845a683e9c88d47cff68aef9a4c38b551
parathinker <100070252+dulhara19@users.noreply.github.com>
2023-07-30 21:35:11 +0530

    Add files via upload

---
+++ Tutorial_11/q1.php
@@ -0,0 +1,17 @@
+<html>
+    <body>
+        <?php 
+        
+        $x=6;
+        $y=4;
+        $sum= $x+$y ;
+        $dif=$x-$y;
+        $pro=$x*$y;
+        $div=$x/$y;
+         echo " sum is : ".$sum; 
+         echo " <br> difference is : ".$dif ;
+         echo " <br> product is : ".$pro;
+         echo " <br> division is : ".$div;
+        ?>
+    </body>
+</html>
---
+++ Tutorial_11/q2.php
@@ -0,0 +1,15 @@
+<html>
+    <body>
+    <?php
+
+    
+    for($i=5;$i<=15;$i++){
+
+        echo $i." ";
+        
+    }
+   
+    ?>    
+    
+    </body>
+</html>
